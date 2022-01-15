051d14bf1e5230934de5acc3dd0c0eefdc4f6598
Sharan-Saha <sharansaha07@gmail.com>
2022-01-14 20:39:28 -0500

    Added Classes for Mortgage

--- Main.java
+++ Main.java
@@ -1,68 +1,29 @@
 package com.sharan;
 
-import java.security.Principal;
 import java.text.NumberFormat;
-import java.util.Scanner;
 
 public class Main {
+    private static MortgageCalculator calculator;
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+
+
 
     public static void main(String[] args) {
-        NumberFormat formatter = NumberFormat.getCurrencyInstance();
-        int principal = (int)(readNumber("Principal: ", 1000, 1_000_000));
-        double annualInterestRate = readNumber("Annual interest rate: ", 1, 30);
-        byte lengthOfLoan= (byte)(readNumber("Length of loan: ", 1, 30 ));
+        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
-        int numOfPayments = 12 * (int)lengthOfLoan;
-
-        printPaymentSchedule(formatter, principal, annualInterestRate, lengthOfLoan, numOfPayments);
-
-//        String moneyString = formatter.format(calculateMortgage(principal, annualInterestRate, lengthOfLoan));
-//        System.out.println(moneyString);
-
-        //System.out.println(getRemBalance(100000, 3.92, 360, 360));
-    }
-
-    private static void printPaymentSchedule(NumberFormat formatter, int principal, double annualInterestRate, byte lengthOfLoan, int numOfPayments) {
-        String paymentString = formatter.format(calculateMortgage(principal, annualInterestRate, lengthOfLoan));
-        System.out.println("Monthly Payment: " + paymentString);
-
-        System.out.println("------------------------");
-        System.out.println("PAYMENT SCHEDULE");
-        System.out.println("------------------------");
-
-        for (int i = 1; i < numOfPayments + 1; i++) {
-            String remBalanceString = formatter.format(getRemBalance(principal, annualInterestRate, numOfPayments, i));
-
-            System.out.println("Payment " + i + ": " + remBalanceString);
+        var calculator = new MortgageCalculator(principal, annualInterest, years);
+        calculator.printMortgage();
+        
+        for (double balance : calculator.getRemainingBalances()) {
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
         }
+
+
     }
 
-    public static double calculateMortgage(int principal, double annualInterest, byte years){
-        double monthlyInterestRate = annualInterest / 100/ 12;
-        int numOfPayments = years * 12;
 
-        return ((monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), numOfPayments))) / ((Math.pow((1 + monthlyInterestRate), numOfPayments) - 1))) * principal;
-    }
-    public static double readNumber(String prompt, double min, double max) {
-        Scanner scanner = new Scanner(System.in);
-        double value;
-        while (true) {
-            System.out.print(prompt);
-            value = scanner.nextFloat();
-            if (value >= min && value <= max) {
-                break;
-            }
-            System.out.println("Enter a value between " + min + "and" + max);
-        }
-        return value;
-    }
-    public static double getRemBalance(int principal, double annualInterestRate, int numPayments, int paymentsMade) {
-        int PERCENT = 100;
-        int monthsInYear = 12;
-        double monthlyInterestRate = annualInterestRate / PERCENT / monthsInYear;
-        double numerator = (Math.pow((1 + monthlyInterestRate), numPayments)) - (Math.pow((1 + monthlyInterestRate), paymentsMade));
-        double denominator = (Math.pow((1 + monthlyInterestRate), numPayments)) -1;
 
-        return (numerator / denominator) * principal;
-    }
 }
